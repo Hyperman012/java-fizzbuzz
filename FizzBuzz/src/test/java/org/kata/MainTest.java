2b63a910fb01e62ccbc4c554f1a8f5a3f142c869
Adam Whaley <adam@industriallogic.com>
2022-11-02 08:04:47 -0500

    initial setup

---
+++ FizzBuzz/src/test/java/org/kata/MainTest.java
@@ -0,0 +1,17 @@
+package org.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+
+
+
+
+}
