ec809f4266ed4c6bf899b81c51be7df76d4ccc50
Adam Whaley <adam@industriallogic.com>
2023-02-16 07:42:44 -0600

    refactor

--- FizzBuzz/src/main/java/org/kata/Main.java
+++ FizzBuzz/src/main/java/org/kata/Main.java
@@ -4,12 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i <= 100; i++) {
-            System.out.println(calculate(i));
+            System.out.println(i);
         }
     }
 
-    public static String calculate(int value) {
-        return null;
-    }
 }
 
