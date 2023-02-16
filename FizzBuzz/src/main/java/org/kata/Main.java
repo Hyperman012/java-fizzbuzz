0385fca4be6721f9232556bc9e57847dc2fd0f89
Adam Whaley <adam@industriallogic.com>
2023-02-16 07:41:35 -0600

    add for loop

--- FizzBuzz/src/main/java/org/kata/Main.java
+++ FizzBuzz/src/main/java/org/kata/Main.java
@@ -3,6 +3,13 @@ package org.kata;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(calculate(i));
+        }
+    }
 
+    public static String calculate(int value) {
+        return null;
     }
 }
+
--- FizzBuzz/src/test/java/org/kata/MainTest.java
+++ FizzBuzz/src/test/java/org/kata/MainTest.java
@@ -11,7 +11,4 @@ class MainTest {
         assertTrue(true);
     }
 
-
-
-
 }
