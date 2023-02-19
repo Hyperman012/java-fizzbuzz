22db78850dfc34eb791a9c81ad96f3701bbed4cf
Adam Whaley <adam@industriallogic.com>
2023-02-18 19:50:30 -0600

    add solution

--- FizzBuzz/src/main/java/org/kata/Main.java
+++ FizzBuzz/src/main/java/org/kata/Main.java
@@ -4,9 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i <= 100; i++) {
-            System.out.println(i);
+            System.out.println(calculate(i));
         }
     }
 
+    public static  String calculate(int input) {
+        if(isMultipleOf(3, input) && isMultipleOf(5, input))
+            return "fizzbuzz";
+        if(isMultipleOf(3, input))
+            return "fizz";
+        if(isMultipleOf(5, input))
+            return "buzz";
+
+        return String.valueOf(input);
+    }
+    private static boolean isMultipleOf(int multiple, int value) {
+        return value % multiple == 0;
+    }
 }
 
--- FizzBuzz/src/test/java/org/kata/MainTest.java
+++ FizzBuzz/src/test/java/org/kata/MainTest.java
@@ -1,14 +1,30 @@
 package org.kata;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
-    @Test
-    void main() {
-        assertTrue(true);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2,4,97})
+    void Calculate_ReturnsStringForNumbers(int number) {
+        assertEquals(String.valueOf(number), Main.calculate(number));
     }
-
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6,9})
+    void Calculate_ReturnsFizzForMultipliesOf3(int number) {
+        assertEquals("fizz", Main.calculate(number));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10,20})
+    void Calculate_ReturnsBuzzForMultipliesOf5(int number) {
+        assertEquals("buzz", Main.calculate(number));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {15, 30})
+    void Calculate_ReturnsFizzBuzzForMultipliesOf15(int number) {
+        assertEquals("fizzbuzz", Main.calculate(number));
+    }
 }
